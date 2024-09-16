f4e806fa187c96587bdf71d6e930a383a6d2dff0
LIMASSOU HAMAN BELLO <149891433+01329582993@users.noreply.github.com>
2024-09-16 18:56:31 +0600

    Add files via upload
    Lab 1 Task 2 data structure

--- 220041158_T02L01_1B.cpp
+++ 220041158_T02L01_1B.cpp
@@ -1,57 +1,65 @@
-#include <iostream>
-#include <stack>
-#include <string>
-#include <vector>
+#include<iostream>
+#include<stack>
+#include<sstream>
+#include<string>
 
-//
-//<html>
-//  <head>
-//    <title> title of webpage </title>
-//  </head>
-//  <body>
-//    <p> This is a <b>paragraph</b> </p>
-//  </body>
-//</html>
-//-1
-
-//int n;
-//vector<int>a;
-//while(cin >> n)
-//{
-//    a.push_back(n);
-//}
-//
-
+using namespace std;
 int main()
 {
-    char i;
-    std::string code;
-    std::string temp;
-    std::string balise;
-
-
-    for(int i=0 ; i<5; i++)
+    stack<string> st;
+    string temp,line;
+    int n,err,read=0,check=0,firstOcc=0;
+    cin>>n;
+    cin.ignore();
+    for(int j=0; j<=n; j++)
     {
-        std::cin>>code[i];
-    }
-
-    for(char ch::code)
-    {
-
-        temp.clear();
-        if(ch == '<')
+        getline(cin,line);
+        for(int i=0; i<line.size(); i++)
         {
-            temp += ch;
-            if(ch == '>')
+            if(line[i]=='<')
             {
-                balise = temp;
-                temp.clear();
+                read=1;
+                temp=line[i+1];
+                continue;
+            }
+            if(line[i+1]=='>')
+            {
+                read=0;
+                check=1;
+            }
+            if(read)
+            {
+                temp+=line[i+1];
+            }
+            if(check)
+            {
+                if(temp[0]!='/')
+                {
+                    st.push(temp);
+                    check=0;
+                }
+                else
+                {
+                    temp.erase(0,1);
+                    if(temp==st.top())
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        if(firstOcc==0)
+                        {
+                            err=j+1;
+                            firstOcc=1;
+                        }
+                    }
+                    check=0;
+                }
             }
         }
-        std::cout<<balise[0];
 
     }
-
-
+    if(firstOcc)
+        cout<<"Error at line "<<err<<endl;
     return 0;
 }
